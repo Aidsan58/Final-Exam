23cbb70cc59b7d5381a584c9fcd6774422cf63e0
Aidsan58 <aidanr1@comcast.net>
2025-08-20 19:28:21 -0700

    Remade repository.

---
+++ reading_frame.py
@@ -0,0 +1,4 @@
+def reading_frame(filename):
+    """ A reading frame is a way of dividing the DNA sequence of nucleotides into codons,
+which are defined as consecutive, non-overlapping triplets. An open reading frame, or ORF, can encode a protein.
+It starts with a start codon (ATG), and ends with a stop codon (TAA; TAG; TGA). """
